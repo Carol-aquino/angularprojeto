b9b1fb8af5f70c7666577a4265de04a941414e6d
Carol-aquino <pinhocarolinapinhoaquino@gmail.com>
2026-06-26 15:29:59 -0300

    Primeiro commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './componentes/home/home';
+import { Contato } from './componentes/contato/contato';
+import { Lancamento } from './componentes/lancamento/lancamento';
+import {Calc} from './componentes/calc/calc';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:"", redirectTo:"home", pathMatch:"full" },
+    { path: "home",component:Home},
+    { path: "contato",component:Contato},
+    { path: "lancamento",component:Lancamento},
+    { path: "calculadora",component:Calc}
+
+];
